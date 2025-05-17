5e0b06436387c8ab0b8be0e413a946ab7455d7e5
rmusayevr <rmusayevr19@gmail.com>
2025-05-17 12:36:27 +0200

    fix: Updated tests for calculator

--- src/pytemplate/service/calculator.py
+++ src/pytemplate/service/calculator.py
@@ -14,4 +14,4 @@ class Calculator:
     def divide(self, operands: Operands) -> int:
         if operands.second_operand == 0:
             raise ValueError("Cannot divide by zero.")
-        return operands.first_operand // operands.second_operand  # integer division
+        return operands.first_operand // operands.second_operand
