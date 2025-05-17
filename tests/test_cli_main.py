813781c87a188a454aec67da2179404ec0ce14fa
rmusayevr <rmusayevr19@gmail.com>
2025-05-17 12:45:41 +0200

    fix: Updated mock.patch

--- tests/test_cli_main.py
+++ tests/test_cli_main.py
@@ -1,39 +1,39 @@
 import builtins
-from unittest.mock import patch
+from unittest import mock
 
 from src.pytemplate.entrypoints.cli.main import main
 
 
 def test_add(capsys):
-    with patch.object(builtins, "input", side_effect=["45", "35", "add"]):
+    with mock.patch.object(builtins, "input", side_effect=["45", "35", "add"]):
         main()
     captured = capsys.readouterr()
     assert "Result: 80" in captured.out
 
 
 def test_subtract(capsys):
-    with patch.object(builtins, "input", side_effect=["100", "40", "subtract"]):
+    with mock.patch.object(builtins, "input", side_effect=["100", "40", "subtract"]):
         main()
     captured = capsys.readouterr()
     assert "Result: 60" in captured.out
 
 
 def test_multiply(capsys):
-    with patch.object(builtins, "input", side_effect=["7", "6", "multiply"]):
+    with mock.patch.object(builtins, "input", side_effect=["7", "6", "multiply"]):
         main()
     captured = capsys.readouterr()
     assert "Result: 42" in captured.out
 
 
 def test_divide(capsys):
-    with patch.object(builtins, "input", side_effect=["90", "10", "divide"]):
+    with mock.patch.object(builtins, "input", side_effect=["90", "10", "divide"]):
         main()
     captured = capsys.readouterr()
     assert "Result: 9" in captured.out
 
 
 def test_invalid_action():
-    with patch("builtins.input", side_effect=["5", "5", "invalid"]):
+    with mock.patch("builtins.input", side_effect=["5", "5", "invalid"]):
         try:
             main()
             assert False, "Should raise ValueError"
